999fbe569bb8994b654f96954ee76f9e9d726eb7
落山义 <1393579810@qq.com>
2023-04-23 21:23:40 +0800

    boxjs测试

--- main.sh
+++
@@ -1,7 +0,0 @@
-# rm -rf alist* #取消注释此行以更新
-如果[ ! -f "alist" ];那么
-curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
-tar -zxvf alist.tar.gz
-rm -f alist.tar.gz
-fi
-./alist服务器--无前缀
